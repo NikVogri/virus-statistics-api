46d36a0df867174fb1fded327ef679abcdd74188
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-03-27 00:38:15 +0100

    added protection middlewares

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors());
 
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 1 // limit each IP to 100 requests per windowMs
+  max: 50 // limit each IP to 100 requests per windowMs
 });
 
 app.use(limiter);
