e7175b74fc61e6fa3c4cd83f1175cd83d25109f3
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-03-27 00:36:14 +0100

    added protection middlewares

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const app = require("express")();
 const connectToDb = require("./database");
 const dataRoute = require("./routes/dataRoute");
 const errorHandler = require("./middleware/errorHandler");
+const rateLimit = require("express-rate-limit");
+const cors = require("cors");
+const helmet = require("helmet");
 
 if (process.env.NODE_ENV === "development") {
   require("dotenv").config({
@@ -9,9 +12,18 @@ if (process.env.NODE_ENV === "development") {
   });
 }
 
+app.use(helmet());
+app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 1 // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+
 app.use("/api/v1", dataRoute);
 app.use(errorHandler);
-
 const port = process.env.PORT || 3000;
 connectToDb();
 app.listen(port, () => {
